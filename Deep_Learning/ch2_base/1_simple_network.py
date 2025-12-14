b95d29ce191147898e1570919198d6bfa9117005
kejun <ckj119940887@gmail.com>
2025-12-14 17:12:06 -0600

    deep learning start

---
+++ Deep_Learning/ch2_base/1_simple_network.py
@@ -0,0 +1,48 @@
+import numpy as np
+import sys
+import os
+
+from pip._internal import network
+
+sys.path.append(
+    os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+)
+from common.functions import sigmoid, identity
+
+def init_network():
+    network = {}
+    network ["W1"] = np.array(([0.1, 0.3, 0.5], [0.2, 0.4, 0.6]))
+    network ["b1"] = np.array([0.1, 0.2, 0.3])
+
+    network ["W2"] = np.array(([0.1, 0.4], [0.2, 0.3], [0.3, 0.6]))
+    network ["b2"] = np.array([0.1, 0.2])
+
+    network ["W3"] = np.array(([0.1, 0.3], [0.2, 0.4]))
+    network ["b3"] = np.array([0.1, 0.2])
+
+    return network
+
+def forward(network, X):
+    W1, W2, W3 = network["W1"], network["W2"], network["W3"]
+    b1, b2, b3 = network["b1"], network["b2"], network["b3"]
+
+    a1 = np.dot(X, W1) + b1
+    z1 = sigmoid(a1)
+
+    a2 = np.dot(z1, W2) + b2
+    z2 = sigmoid(a2)
+
+    a3 = np.dot(z2, W3) + b3
+    z3 = sigmoid(a3)
+
+    y = identity(z3)
+    return y
+
+# test
+X = np.array([1, 0.5])
+
+# model
+network = init_network()
+
+y = forward(network, X)
+print(y)
---
+++ Deep_Learning/common/functions.py
@@ -0,0 +1,49 @@
+import numpy as np
+
+# 传入标量
+def step_function0(x):
+    if x >= 0:
+        return 1
+    else:
+        return 0
+
+# 传入向量或矩阵
+def step_function(x):
+    return np.array(x >= 0, dtype=int)
+
+# sigmoid
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+# RELU
+def relu(x):
+    return np.maximum(0, x)
+
+# softmax
+# 输入为向量
+def softmax0(x):
+    # prevent overflow
+    x = x - np.max(x)
+    e_x = np.exp(x) / np.sum(np.exp(x))
+    return e_x
+
+# 输入为矩阵
+def softmax(x):
+    x = x.T
+    x = x - np.max(x, axis=0)
+    e_x = np.exp(x) / np.sum(np.exp(x), axis=0)
+    return e_x.T
+
+# Identity
+def identity(x):
+    return x
+
+# leaky RELU
+
+
+# test
+x = np.array([0, 1, 2, 3, 4, 5, -6, -7, -8, -9, -10])
+# print(step_function(x))
+# print(sigmoid(x))
+# print(np.tanh(x))
+# print(relu(x))
